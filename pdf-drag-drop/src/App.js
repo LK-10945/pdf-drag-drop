7a7b32d00ae19fb8e1aed31565b14c4f73acf604
LK-10945 <lucasckahn@gmail.com>
2025-08-18 18:26:27 -0800

    Add files via upload

---
+++ pdf-drag-drop/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PDFDropZone from './PDFDropZone';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Class Submissions Page Upload</h1>
+      <PDFDropZone />
+    </div>
+  );
+}
+
+export default App;
